b39f00772344c8c7ee150bb134e64ae2c5865720
Nicolás Salazar <nicolas.salazar@ncodes.co>
2020-10-07 18:02:39 -0500

    Now effects are attached into effects file and is imported into main index.js

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-function setPopEffectOnCards() {
-    let myItems = document.getElementsByClassName('centered-container tricki-cell');
-    for (let i = 0; i < myItems.length; i++) {
-        let myCard = myItems[i];
-
-        myCard.addEventListener('mouseover', () => {
-            myCard.classList.add("onhover-card");
-        });
-
-        myCard.addEventListener('mouseout', () => {
-            myCard.classList.remove("onhover-card");
-        });
-    }
-}
-
-setPopEffectOnCards();
+import myEffectsUtils from './scripts/effects';
+myEffectsUtils.setCardsPopEffect();
