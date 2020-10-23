bb1533807e12842099c27e8e93bf585813494295
Nicolás Salazar <nicolas.salazar@ncodes.co>
2020-10-23 17:20:25 -0500

    Built logics for play. Bugs found

--- index.js
+++ index.js
@@ -1,26 +1,75 @@
+const BACK_CARD_IMAGE = 'https://opengameart.org/sites/default/files/card%20back%20red.png';
+
+let activeCard = null;
+
+function printActiveCardValue(){
+    console.log('Active card: ', activeCard);
+}
+
 function appendCardToParent(parent, cardData) {
     let myNewCard = document.createElement('div');
     myNewCard.className = "col-12 col-md-3 game-card";
-    myNewCard.style = "background-image: url(" + cardData.image + ")";
+    myNewCard.style = "background-image: url(" + BACK_CARD_IMAGE + ")";
     myNewCard.cardValue = cardData.value;
+    myNewCard.cardImage = cardData.image;
+    myNewCard.showCard = false;
 
-    myNewCard.addEventListener('click',()=>{
-        console.log('Pressed card with value: ',cardData.value);    
+    myNewCard.addEventListener('click', () => {
+        if(myNewCard.showCard === true){
+            //myNewCard.style = "background-image: url(" + BACK_CARD_IMAGE + ")";
+            myNewCard.showCard = false;
+        }
+        else{
+            myNewCard.style = "background-image: url(" + myNewCard.cardImage + ")";
+            myNewCard.showCard = true;
+        }
+
+        if(!activeCard){
+            activeCard = myNewCard.cardValue;
+            console.log('CARTA ACTIVA -> ', activeCard);
+        }
+        else{
+            if(activeCard === myNewCard.cardValue){
+                activeCard = null;
+            }
+            else{
+                setTimeout(() => {
+                    swipCardsBackByValue(myNewCard.cardValue);
+                    swipCardsBackByValue(activeCard);
+                    activeCard = null;
+                }, 1000);
+            }
+        }
+
     });
 
     parent.appendChild(myNewCard);
 }
 
+function swipCardsBackByValue(value) {
+    console.log('Swipping back all cards with: ', value);
+    let myCards = document.getElementsByClassName('col-12 col-md-3 game-card');
+
+    for (let i = 0; i < myCards.length; i++) {
+        let myCard = myCards[i];
+
+        if (myCard.cardValue === value) {
+            myCard.style = "background-image: url(" + BACK_CARD_IMAGE + ")";
+        }
+    }
+
+}
+
 function createCards(deckId) {
-    fetch('https://deckofcardsapi.com/api/deck/'+deckId+'/draw/?count=6', {
+    fetch('https://deckofcardsapi.com/api/deck/' + deckId + '/draw/?count=6', {
         method: 'GET'
     })
         .then(serverResponse => {
             serverResponse.json()
                 .then(apiResult => {
-                    
-                    let cardsArray =[];
-                    
+
+                    let cardsArray = [];
+
                     apiResult.cards.forEach((item, i) => {
                         cardsArray.push({
                             image: item.image,
@@ -33,7 +82,7 @@ function createCards(deckId) {
                         });
                     });
 
-                    cardsArray = cardsArray.sort( () => .5 - Math.random() );
+                    cardsArray = cardsArray.sort(() => .5 - Math.random());
 
                     let cardsContainer = document.getElementById('cardsContainer')
                     cardsArray.forEach(card => {
@@ -50,7 +99,7 @@ function createCards(deckId) {
         });
 }
 
-function buildGame(){
+function buildGame() {
     fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1', {
         method: 'GET'
     })
